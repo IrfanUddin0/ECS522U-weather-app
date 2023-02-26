17c577f6b569a9a306349f391e56eb3a7c5ec9d7
IrfanUddin0 <irfanniddu@gmail.com>
2023-02-26 23:24:26 +0000

    init commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+// import preact
+import { h, Component } from 'preact';
+
+// import required Components from 'components/'
+import Home from './Home';
+
+export default class App extends Component {
+	render(){
+		return (
+			<Home/>
+		);
+	}
+}
